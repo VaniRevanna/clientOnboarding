00de349aa2795e9568cfaf39f9cfde83ecb15857
vani <vani.br@vida.id>
2024-05-16 16:23:50 +0530

    initial commit

---
+++ src/main/java/com/precissionTest/UserOnBaording/UserOnBaordingApplication.java
@@ -0,0 +1,13 @@
+package com.precissionTest.UserOnBaording;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOnBaordingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserOnBaordingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/precissionTest/UserOnBaording/UserOnBaordingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precissionTest.UserOnBaording;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserOnBaordingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
